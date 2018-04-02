9aa92bc3f0ae1891c2ccd09cde2ced18aeb68b57
Justin Angevaare <jangevaa@uoguelph.ca>
2018-04-02 13:10:53 -0400

    layout improvements

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ description = '''
 ### Background
 The statistical analysis associated with the triangle test compares the proportion of test participants that have correctly identified an odd sample out, to the proportion of tasters that would be expected to correctly identify the odd sample purely due to random chance. The greater the proportion of correct participants, the more evidence there is against the “random chance” null hypothesis. As we are only interested if the different beers can be correctly distinguished, this will be an “upper tailed” test. That is to say, the potential result of the odd sample out being correctly identified less than we’d expect under random chance isn’t of relevance.
 
-In plain terms, our null hypothesis is that the true proportion of population that can correctly identify the odd sample out is 1/3. Our alternative hypothesis is that the true proportion of the population that can correctly identify the odd sample out is greater than 1/3.
+In plain terms, our null hypothesis, Η₀, is that the true proportion of population that can correctly identify the odd sample out is 1/3. Our alternative hypothesis, H₁, is that the true proportion of the population that can correctly identify the odd sample out is greater than 1/3.
 
 ### Exact p-value
 The exact p-value can be calculated using the binomial distribution. Specifically, the p-value is found as the probability of having observed at least as many correct tasters, if the population proportion is infact equal to 1/3. This calculation is implemented in the right pane of this web app.
@@ -35,12 +35,12 @@ attribution = '''
 Built by [Justin Angevaare](http://onuncertainty.com) with [Plot.ly Dash](https://dash.plot.ly)
 '''
 
-app.title = 'Triangle Test Calculator'
+app.title = 'TriangleDash'
 app.layout = html.Div(className = 'twelve columns', children=[
 html.Script(type="text/javascript", src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"),
     html.Div(className='row', children=[
         html.Center(
-            html.H1('Triangle Test Calculator')
+            html.H1('TriangeDash: Triangle Test Calculator and Utilities')
             )
         ]),
     html.Div(className='row', children=[
@@ -48,26 +48,32 @@ html.Script(type="text/javascript", src="http://cdn.mathjax.org/mathjax/latest/M
             dcc.Markdown(children=description)
             ]), # end of left side
         html.Div(className='six columns', children=[
+            html.H3('Calculator'),
             html.Div(className='row', children=[
-                dcc.Graph(id='plot')
-                ]), # end of row
-            html.Div(className='row', children=[
-                html.Div(className='four columns', children=[
-                    html.Label('Number of participants'),
+                html.Div(className='five columns', children=[
+                    html.Label('Number of participants:', style={'verticalAlign':'middle'})
+                    ]),
+                html.Div(className='seven columns', style={'textAlign': 'left'}, children=[
                     dcc.Input(id='size', type='number', value=20)
-                    ]),
-                html.Div(className='four columns', children=[
-                    html.Label('Number of correct participants'),
-                    dcc.Input(id='correct', type='number', value=10)
-                    ]),
-                html.Div(className='four columns', children=[
-                    html.Br(),
-                    html.Button('Submit', id='button')
                     ])
-                ]), # end of row
+                ]),
+                html.Br(),
+                html.Div(className='row', children=[
+                    html.Div(className='five columns', children=[
+                        html.Label('Number of correct participants:', style={'verticalAlign':'middle'})
+                        ]),
+                    html.Div(className='seven columns', style={'textAlign': 'left'}, children=[
+                        dcc.Input(id='correct', type='number', value=10)
+                        ])
+                    ]),
+            html.Div(className='row', style={'text-align': 'left'}, children=[
+                html.Br(),
+                html.Button('Submit', id='button')
+                ]),
             html.Div(className='row', children=[
-                html.H3(id='exact')
-                ]) # end of row
+                html.H3('Result'),
+                dcc.Graph(id='plot')
+                ])
             ]) # end of right side
         ]),
     html.Div(className='row', children=[
@@ -104,22 +110,17 @@ def makeplot(clicks, n, correct):
                 hoverinfo='none'
                 )],
         layout = go.Layout(
-            title='',
+            title=exactp(clicks, n, correct),
             barmode='stack',
             showlegend=False,
             bargap = 0.0,
-            yaxis = dict(fixedrange = True),
-            xaxis = dict(fixedrange = True)
+            yaxis = dict(fixedrange = True,
+                         title='Event probability under H₀'),
+            xaxis = dict(fixedrange = True,
+                         title='Number of correct participants'),
             )
         )
 
-
-@app.callback(
-    Output(component_id='exact', component_property='children'),
-    [Input(component_id='button', component_property='n_clicks')],
-    [State(component_id='size', component_property='value'),
-    State(component_id='correct', component_property='value')]
-    )
 def exactp(clicks, n, correct):
     if n == 0:
         return "Must have at least one participant"
